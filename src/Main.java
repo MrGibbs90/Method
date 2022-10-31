3fe66294a01417a86802a9c8d280985559f9e4c0
MrGibbs90 <mrgibbs9090@mail.ru>
2022-10-31 20:06:33 +0700

    Методы

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,72 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        task3();
+    }
+
+    public static void printIsLeapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+
+    private static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
+    }
+
+    public static void task1() {
+        int year = 2005;
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print("lite ");
+        }
+        System.out.print("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else {
+            System.out.println("Android");
+        }
+    }
+
+    public static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear < currentYear;
+    }
+
+    public static void task2() {
+        int clientOS = 0;
+        int deviceYear = 2022;
+        int currentYear = 2022;
+        recommendApplicationVersion(clientOS, deviceYear);
+    }
+
+    public static void calculateDeliveryDays(int deliveryDistance, int deliveryDays) {
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+    }
+    public static void task3() {
+        int deliveryDays = 1;
+        int deliveryDistance = 70;
+        calculateDeliveryDays(deliveryDistance, deliveryDays);
     }
 }
